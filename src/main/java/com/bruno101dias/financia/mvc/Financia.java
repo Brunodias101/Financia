ee8afefd1e982aac598a191716cd5a5da0ab4943
brunodias101 <bruno101dias@gmail.com>
2026-08-06 13:52:12 -0300

    chore: setup inicial do projeto Financia (Spring Boot + Maven)

---
+++ src/main/java/com/bruno101dias/financia/mvc/Financia.java
@@ -0,0 +1,13 @@
+package com.bruno101dias.financia.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Financia {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Financia.class, args);
+	}
+
+}
